4c6ff762dfbecf47c883bb4522ba517cd9bd22a9
hky035 <rldud035@naver.com>
2024-08-17 21:17:13 +0900

    feat: button 공통 컴포넌트 생성

---
+++ src/component/common/Button.jsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const Button = styled.button`
+    width: 100%;
+    border-radius : 6px;
+    color : #fff;
+    font-size : 20px;
+    font-weight : 600;
+    background-color : var(--main-color);
+    box-sizing : border-box;
+    padding : 10px 0;
+    cursor: pointer;
+`
+
+export default Button;
+
